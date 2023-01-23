32888d0017898db7849095f16782093f6c021e8f
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-23 14:40:11 +0100

    added print_list_reverse function

--- print_list.c
+++ print_list.c
@@ -17,7 +17,30 @@ void print_list(const listint_t *list)
 			printf(", ");
 		printf("%d", list->n);
 		++i;
+	list = list->next;
+	}
+	printf("\n");
+}
+
+/**
+ * print_list_reverse - Prints a list of integers in reverse
+ *
+ * @list: The list to be printed
+ */
+void print_list_reverse(const listint_t *list)
+{
+	int i;
+
+	i = 0;
+	while (list->next)
 		list = list->next;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->prev;
 	}
 	printf("\n");
 }
--- sort.h
+++ sort.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -20,6 +19,7 @@ typedef struct listint_s
 
 void print_array(const int *, size_t);
 void print_list(const listint_t *);
+void print_list_reverse(const listint_t *);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
