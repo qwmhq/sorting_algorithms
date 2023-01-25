8af198e03f864ad9f23e3998beea6930e5cfe61f
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-25 21:16:05 +0100

    task 5 - cocktail shaker sort

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - swap two adjacent nodes in a
+ * doubly linked list
+ *
+ * @list: the head of the list
+ * @ptr: node in the list
+ * @next: node after ptr
+ */
+void swap_nodes(listint_t **list, listint_t *ptr, listint_t *next)
+{
+	next->prev = ptr->prev;
+	ptr->next = next->next;
+	ptr->prev = next;
+	next->next = ptr;
+	if (ptr->next != NULL)
+		ptr->next->prev = ptr;
+	if (next->prev != NULL)
+		next->prev->next = next;
+	else
+		*list = next;
+}
+
+/**
+ * cocktail_sort_list - sort a doubly linked list of
+ * integers in ascending order using the cocktail shaker
+ * sort algorithm
+ *
+ * @list: the list to sort
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *ptr;
+	int swapped;
+
+	do {
+		swapped = 0;
+		ptr = *list;
+		while (ptr != NULL && ptr->next != NULL)
+		{
+			if (ptr->n > ptr->next->n)
+			{
+				swap_nodes(list, ptr, ptr->next);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				ptr = ptr->next;
+		}
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		while (ptr->prev != NULL)
+		{
+			if (ptr->n < ptr->prev->n)
+			{
+				swap_nodes(list, ptr->prev, ptr);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				ptr = ptr->prev;
+		}
+	} while (swapped == 1);
+
+}
